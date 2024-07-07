f8b7d29066a7a93aae422e84301b6ea1e66a4d50
ADMIN <tiendong2kk2@gmail.com>
2024-07-07 15:23:14 +0700

    done

---
+++ app/src/main/java/com/example/b21/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.b21
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.b21.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // set color icon
+        binding.navLeftMenu.itemIconTintList = null
+
+        binding.navLeftMenu.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                    true // Correctly return a Boolean value
+                }
+
+                R.id.nav_message -> {
+                    Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show()
+                    true
+
+                }
+
+                R.id.nav_exit -> {
+                    finish()
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+}
